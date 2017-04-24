7c0a35ca9d64a8c01cba9b3e30730024b85233df
thikhin96 <nghoangthi1@gmail.com>
2017-04-25 00:01:25 +0700

    first version with merely html

---
+++ src/main.js
@@ -0,0 +1,26 @@
+const electron = require('electron');
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+const path = require('path');
+const url = require('url');
+
+let mainWindow
+
+
+app.on('ready', _=>{
+	mainWindow = new BrowserWindow({
+		height: 780,
+		width: 1280,
+		resizable: false,
+	});
+	
+	mainWindow.loadURL(url.format({
+    pathname: path.join(__dirname, 'home.html'),
+    protocol: 'file:',
+    slashes: true
+  }));
+
+	mainWindow.on('closed', _=>{
+		mainWindow = null;
+	})
+});
