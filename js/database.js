e77d0a6a57b20a18d2a02e6552432b3e1ddd91bc
thaonguyen1096 <thaonguyen1096@gmail.com>
2017-04-28 05:35:06 +0700

    add-database

--- js/database.js
+++ js/database.js
@@ -14,6 +14,7 @@ exports.addUser = function(username, password, lastPeriod, mentsCycle, avgPeriod
     user.put();
 };
 
+//thêm kỳ kinh
 exports.addPeriod = function(username, start, end)
 {
     var period1 = {
@@ -24,11 +25,13 @@ exports.addPeriod = function(username, start, end)
     };
     period.put(period1).then(function(response){
       console.log("success", response)
+      alert("Đã thêm kỳ kinh!");
     }).then(function(err){
       console.log("Error", err)
     });
 };
 
+//tất cả các kỳ kinh
   exports.all_period = function() {
       // Get all period from the database
       period.allDocs({include_docs: true}).then(function (docs) {
@@ -41,11 +44,54 @@ exports.addPeriod = function(username, start, end)
             // console.log("docs thứ mấy k biết nữa" + docs.rows[i].doc._id);
             all_start += '<option id ="option" value="' + docs.rows[i].doc.start + '">' + docs.rows[i].doc.start+'</option>'
           }
-          // console.log(all_start);
+
           // Fill the table content
           document.getElementById('all_start').innerHTML = all_start;
         });
 };
+
+//tìm ngày kết thúc và kỳ kinh kéo dài
+  exports.find_endday = function(username, start){
+    period.get(username+start).then(function(doc){
+      //in ra ngày kết thúc
+      document.getElementById('end').value = doc.end;
+
+      //in ra kỳ kinh kèo dài
+      var end = document.getElementById("end").value;
+      var oneDay = 24*60*60*1000; // hours*minutes*seconds*milliseconds
+      var x = start.split("/");
+      var y = end.split("/");
+      var s = new Date(x[2], x[1] - 1, x[0]);
+      var e = new Date(y[2], y[1] - 1, y[0]);
+      var kykinh = Math.round((e.getTime() - s.getTime())/oneDay) + 1;
+      document.getElementById("kykinh").innerHTML = kykinh;
+    });
+  };
+
+//chỉnh sửa kỳ kinh
+  exports.updatePeriod = function(username, start, end){
+    period.get(username+start)
+      .then(function(doc) {
+        doc.end = end// new field
+        // console.log(doc._rev) // doc has a '_rev' field
+        return period.put(doc)   // put updated doc, will create new revision
+      }).then(function (res) {
+        alert("Cập nhật thành công!")
+        console.log(res)
+      })
+  }
+
+//Xóa kỳ kinh
+exports.deletePeriod = function(username, start){
+  period.get(username + start)
+    .then(function (doc) {
+      doc._deleted = true
+      return period.put(doc)
+      alert("Xóa thành công!")
+    })
+}
+
+
 // <select id ="">
 //   <option value="None" >-- Select --</option>
 //
@@ -159,7 +205,7 @@ exports.addPeriod = function(username, start, end)
 //
 
 
-//Xóa
+//Xóa db
 // users_db
 //   .get('user')
 //   .then(function (doc) {
--- js/themkykinh.js
+++ js/themkykinh.js
@@ -1,29 +1,41 @@
 const database = require('../js/database');
+const jquery = require('../js/jquery-1.9.1.js');
+const jqueryui = require('../js/jquery-ui-1.10.1.min.js');
+
+
+//tính chu kỳ kinh kéo dài
+function myFunction() {
+  var oneDay = 24*60*60*1000; // hours*minutes*seconds*milliseconds
+  var x = $("#start").val().split("/");
+  var y = $("#end").val().split("/");
+
+  var start = new Date(x[2], x[1] - 1, x[0]);
+  var end = new Date(y[2], y[1] - 1, y[0]);
+  var kykinh = Math.round((end.getTime() - start.getTime())/oneDay) + 1;
+  document.getElementById("kykinh").innerHTML = kykinh;
+}
+
 
 window.onload = function() {
-
-  // Populate the table
-  //populateTable();
-
   // Add the add button click event
   document.getElementById('hoantat').addEventListener('click', () => {
 
     // Retrieve the input fields
-    //var username = document.getElementById('start');
+    //var username = document.getElementById('id của user bên html');
     var username = "ThaoLua";
-    var start = $("#start").val();
+    var x = $("#start").val();
+    var y = $("#end").val();
+
+    // var start = new Date(x[2], x[1] - 1, x[0]);
+    // var end = new Date(y[2], y[1] - 1, y[0]);
+
     console.log(start);
-    var end = $("#end").val();
     console.log(end);
     // Save the person in the database
-    database.addPeriod(username, start, end);
-
-    
+    database.addPeriod(username, x, y);
     // Reset the input fields
-    // $("#end").val("");
-    // $("#start").val("")
-
-    // Repopulate the table
-    //populateTable();
+    $("#end").val("");
+    $("#start").val("")
+    document.getElementById("kykinh").value = "";
   });
 }
